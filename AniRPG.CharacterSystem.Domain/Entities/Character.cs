6131b01cc256ce9645600236c6fe290371c72451
famafka <famafka@gmail.com>
2019-07-21 20:40:03 +0300

    Add AniRPG nugets, create Character entity;

---
+++ AniRPG.CharacterSystem.Domain/Entities/Character.cs
@@ -0,0 +1,10 @@
+﻿using AniRPG.Core.Domain;
+
+namespace AniRPG.CharacterSystem.Domain.Entities
+{
+    class Character : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
