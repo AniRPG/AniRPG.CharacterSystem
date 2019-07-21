1c8494ddc36101a657d13bd9fc0cddafdcd21772
famafka <famafka@gmail.com>
2019-07-21 20:50:48 +0300

    Make Character public

--- AniRPG.CharacterSystem.Domain/Entities/Character.cs
+++ AniRPG.CharacterSystem.Domain/Entities/Character.cs
@@ -2,7 +2,7 @@
 
 namespace AniRPG.CharacterSystem.Domain.Entities
 {
-    class Character : IEntity
+    public class Character : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
