fbe57c96ae47e86d56d6efe2ae112a95b07173e0
famafka <famafka@gmail.com>
2019-07-21 22:56:15 +0300

    Add validator for the CreateCharacterCommand

---
+++ AniRPG.CharacterSystem.Game/Constants/CharacterConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace AniRPG.CharacterSystem.Game.Constants
+{
+    public static class CharacterConstants
+    {
+        public const int NameMaxLength = 20;
+        public const int NameMinLength = 3;
+    }
+}
